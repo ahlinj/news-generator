6dc1a6e5a038330724900cd21daa3b3603ea2cc6
Jaka <jakaahlin0@gmail.com>
2025-11-25 18:24:35 +0100

    getting the links to all the articles on t4eu

--- fetch.py
+++ fetch.py
@@ -1,20 +1,31 @@
 import requests
+from bs4 import BeautifulSoup
 
-# test
-
-# t4eu
-
-url = "https://transform4europe.eu/science-communication-workshop-abstract-accepted-and-now-what/"
-
+url = "https://transform4europe.eu/news/"
 response = requests.get(url)
 
-print(response.text)     
+soup = BeautifulSoup(response.text, "html.parser")
+
+results = []
+
+# find all h3 elements with that elementor-post__title class
+for h3 in soup.find_all("h3", class_="elementor-post__title"):
+    # get a link inside the h3
+    a = h3.find("a")
+    if a and a.get("href"):
+        results.append({
+            "title": a.get_text(strip=True),
+            "link": a["href"]
+        })
+
+for r in results:
+    print(r)
 
 # upr
 
-url2="https://www.upr.si/si/o-univerzi/t4eu-/t4eu-novice/pomlad-trajnostnega-razvoja-"
+#url2="https://www.upr.si/si/o-univerzi/t4eu-/t4eu-novice/pomlad-trajnostnega-razvoja-"
 
-response2 = requests.get(url2)
+#response2 = requests.get(url2)
 
-print(response2.text)
+#print(response2.text)
 
