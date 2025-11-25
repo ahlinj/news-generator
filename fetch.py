7aff37b0ddabeb79105df6bbd86e8e55ac46d214
Jaka <jakaahlin0@gmail.com>
2025-11-25 18:38:53 +0100

    getting the links on all the pages on t4eu

--- fetch.py
+++ fetch.py
@@ -1,25 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://transform4europe.eu/news/"
-response = requests.get(url)
 
-soup = BeautifulSoup(response.text, "html.parser")
+BASE_URL = "https://transform4europe.eu/news/"
+MAX_PAGE = 62
 
-results = []
+all_articles = []
 
-# find all h3 elements with that elementor-post__title class
-for h3 in soup.find_all("h3", class_="elementor-post__title"):
-    # get a link inside the h3
-    a = h3.find("a")
-    if a and a.get("href"):
-        results.append({
-            "title": a.get_text(strip=True),
-            "link": a["href"]
-        })
+for page in range(1, MAX_PAGE + 1):
+    # Construct page URL
+    if page == 1:
+        url = BASE_URL
+    else:
+        url = f"{BASE_URL}{page}"
 
-for r in results:
-    print(r)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+   # find all h3 elements with that elementor-post__title class
+    for h3 in soup.find_all("h3", class_="elementor-post__title"):
+        # get a link inside the h3
+        a = h3.find("a")
+        if a and a.get("href"):
+            all_articles.append({
+                "title": a.get_text(strip=True),
+                "link": a["href"]
+            })
+
+print("\nTotal articles collected:", len(all_articles))
+
+for article in all_articles:
+    print(article)
 
 # upr
 
