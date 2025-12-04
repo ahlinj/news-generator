ffa60e421880d583bbb6ea30b4e0d7df98369ee7
Jaka <jakaahlin0@gmail.com>
2025-12-04 13:35:17 +0100

    extracting text from t4eu pages

--- parse_t4eu.py
+++ parse_t4eu.py
@@ -7,6 +7,22 @@ MAX_PAGE = 62
 
 all_articles_t4eu = []
 
+def extract_content(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # article container
+    container = soup.find("div", attrs={"data-elementor-type": "single-post"})
+    
+    if container is None:
+        container = soup.find("div", attrs={"data-elementor-type": "single-page"})
+    
+    # extract all text (paragraphs, headers, links)
+    full_text = container.get_text(separator="\n", strip=True)
+
+    return full_text
+
+
 for page in range(1, MAX_PAGE + 1):
     # Construct page URL
     if page == 1:
@@ -28,6 +44,5 @@ for page in range(1, MAX_PAGE + 1):
             })
 
 for article in all_articles_t4eu:
-    print(article)
-
-print("\nTotal articles collected:", len(all_articles_t4eu))
+    article["content"] = extract_content(article["link"])
+    print(article["content"])
