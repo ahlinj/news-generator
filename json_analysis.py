6526388184f85e3ed166b27795c164281f7c32d3
Jaka <jakaahlin0@gmail.com>
2026-04-19 10:56:36 +0200

    remove null lines from jsonl

--- json_analysis.py
+++ json_analysis.py
@@ -5,12 +5,22 @@ def clean_jsonl(input_path, output_path):
          open(output_path, 'w', encoding='utf-8') as outfile:
 
         records = [json.loads(line) for line in infile]
-        cleaned = [r for r in records 
+        
+        removed = [r for r in records 
                    if '404 article not found (probably)' 
-                   not in r.get('important_links')]
+                   in r.get('important_links')]
+        
+        cleaned = [r for r in records if r not in removed]
 
         outfile.writelines(json.dumps(r, ensure_ascii=False) + '\n' for r in cleaned)
-        print(f"Removed {len(records) - len(cleaned)} records.")
+
+def remove_null_lines(input_path, output_path):
+    with open(input_path, 'r', encoding='utf-8') as f:
+        records = [r for line in f if (r := json.loads(line)) is not None]
+    
+    with open(output_path, 'w', encoding='utf-8') as f:
+        f.writelines(json.dumps(r, ensure_ascii=False) + '\n' for r in records)
 
 if __name__ == "__main__":
     clean_jsonl('data/extracted_soup_links_filtered.jsonl', 'data/extracted_soup_links_filtered_no_404s.jsonl')
+    remove_null_lines('data/extracted_llm_links_filtered_no_common_links.jsonl','data/extracted_llm_links_filtered_no_common_links_no_404s.jsonl')
