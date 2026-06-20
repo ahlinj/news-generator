26d4ed99367f79878cf0af674bc09ff53dcfe0d5
Jaka <jakaahlin0@gmail.com>
2026-06-20 12:12:32 +0200

    extract content from jsonl urls

--- llm_as_a_judge.py
+++ llm_as_a_judge.py
@@ -1,4 +1,6 @@
 import json
+from parse_t4eu import extract_content as extract_content_t4eu
+from parse_upr import extract_content as extract_content_upr
 
 def load_jsonl(path):
     records = []
@@ -9,8 +11,19 @@ def load_jsonl(path):
                 records.append(json.loads(line))
     return records
 
+def extract_content(url):
+    if "transform4europe" in url:
+        return extract_content_t4eu(url)
+    elif "upr" in url:
+        return extract_content_upr(url)
+    
 if __name__ == "__main__":
     llm_records = load_jsonl("data/extracted_llm_links_filtered_no_common_links_1_no_404s.jsonl")
     soup_records = load_jsonl("data/extracted_soup_links_filtered_and_img_5_no_404s.jsonl")
-    print(f"LLM records: {len(llm_records)}")
-    print(f"Soup records: {len(soup_records)}")
+    n = len(llm_records)
+
+    for i in range(n):
+        try:
+            print(f"{i}: {extract_content(soup_records[i]['link'])[:10]}")
+        except Exception as e:
+            print(f"Error occurred while processing record {i}: {e}")
